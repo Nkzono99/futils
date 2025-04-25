d60ee5d068b674663fb9af1312ae0b3459a3ae60
Nkzono99 <210x218x@stu.kobe-u.ac.jp>
2025-04-26 04:05:58 +0900

    Update: Updated a timer module for mpi.

--- src/timer_mpi.f90
+++ src/timer_mpi.f90
@@ -1,17 +1,18 @@
 module m_timer_mpi
-    use, intrinsic :: iso_fortran_env
+    use, intrinsic :: iso_fortran_env, only: dp => real64
     use mpi
 
     implicit none
+
     private
     public :: t_TimerMPI
 
     type :: t_TimerMPI
         integer           :: n_calls = 0
-        real(real64)      :: t0 = 0.0_real64
-        real(real64)      :: tsum = 0.0_real64
-        real(real64)      :: tmin = huge(0.0_real64)
-        real(real64)      :: tmax = 0.0_real64
+        real(dp)      :: t0 = 0.0_dp
+        real(dp)      :: tsum = 0.0_dp
+        real(dp)      :: tmin = huge(0.0_dp)
+        real(dp)      :: tmax = 0.0_dp
     contains
         procedure :: start => tstat_start
         procedure :: stop => tstat_stop
@@ -36,8 +37,10 @@ contains
 
     subroutine tstat_stop(self)
         class(t_TimerMPI), intent(inout) :: self
-        real(real64) :: dt
+        real(dp) :: dt
+
         dt = MPI_Wtime() - self%t0
+
         self%n_calls = self%n_calls + 1
         self%tsum = self%tsum + dt
         self%tmin = min(self%tmin, dt)
@@ -46,6 +49,7 @@ contains
 
     subroutine tstat_reset(self)
         class(t_TimerMPI), intent(inout) :: self
+
         self%n_calls = 0
         self%t0 = 0
         self%tsum = 0
@@ -59,24 +63,20 @@ contains
         integer, intent(in), optional :: comm
 
         integer :: c
-        real(real64)    :: buf(4), g(4)     ! n, tsum, tmin, tmax をまとめて Reduce
-        integer         :: irank
-
+        integer :: irank
         integer :: ierr
 
-        c = MPI_COMM_WORLD
         c = merge(comm, MPI_COMM_WORLD, present(comm))
         call MPI_Comm_rank(c, irank, ierr)
 
-        buf = [real(self%n_calls, real64), self%tsum, self%tmin, self%tmax]
-        call MPI_Reduce(buf, g, 4, MPI_REAL8, MPI_SUM, 0, c)   ! n と tsum は合計
         if (irank == 0) then
-            ! min/max は別途
-            call MPI_Reduce(self%tmin, g(3), 1, MPI_REAL8, MPI_MIN, 0, c)
-            call MPI_Reduce(self%tmax, g(4), 1, MPI_REAL8, MPI_MAX, 0, c)
-
-            write (*, '(a,":  calls=",i8,"  total=",f10.6," s  avg=",f10.6,"  min=",f10.6,"  max=",f10.6)') &
-                trim(merge(label, 'timer', present(label))), nint(g(1)), g(2), g(2)/g(1), g(3), g(4)
+            print *, "------- ", trim(merge(label, 'timer', present(label))), " -------"
+            print *, "calls:", self%n_calls
+            print *, "total:", self%tsum
+            print *, "avg:", self%tsum/self%n_calls
+            print *, "min:", self%tmin
+            print *, "max", self%tmax
+            print *, "---------------------------"
         end if
     end subroutine
 
